a6c9ba2005256d5f41b449e268fa52f377af34bd
kishorkumar.b <kishorkumar.b@agaramtech.com>
2025-11-27 19:05:08 +0530

    27-11-2025

---
+++ react/react-image-upload/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import UploadAndViewImage from "./UploadAndViewImage";
+
+function App() {
+  return (
+    <div className="App">
+      <UploadAndViewImage />
+    </div>
+  );
+}
+
+export default App;
